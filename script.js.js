a1ae20323c1c65c3917392869c7abc2c92edd817
Hamid Ali <hamidaliwork4@gmail.com>
2024-12-17 00:43:20 +0500

    Update script.js.

--- script.js.js
+++ script.js.js
@@ -1,16 +1,9 @@
 "use strict";
-// console.log(
-//   (document.querySelector(".message").textContent = "Correct Number!")
-// );
-// console.log(document.querySelector(".message").textContent);
-// console.log((document.querySelector(".number").textContent = 14));
-// console.log((document.querySelector(".score").textContent = 14));
 
-// console.log((document.querySelector(".guess").value = 14));
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// document.querySelector(".number").textContent = secretNumber;
+
 let highscore = document.querySelector(".highscore").textContent;
 
 document.querySelector(".check").addEventListener("click", function () {
